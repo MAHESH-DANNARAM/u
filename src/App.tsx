4970c558b7cdbef846e75c78b60fd1a3e0d5ccc2
SVINATSAI <svinaysai6@gmail.com>
2023-07-15 20:47:00 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Page from "./page";
 import Page1 from "./page1";
+import Page2 from "./page2";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Page/>} />
       <Route path="/page1" element={<Page1/>} />
+      <Route path="/page2" element={<Page2/>} />
     </Routes>
     
   );
--- src/page1.tsx
+++ src/page1.tsx
@@ -71,7 +71,7 @@ export default function Page1() {
             </li>
             <li>
               <a
-                href="#"
+                href="/page2"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
@@ -91,7 +91,7 @@ export default function Page1() {
             </li>
             <li>
               <a
-                href="#"
+                href="/page2"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
