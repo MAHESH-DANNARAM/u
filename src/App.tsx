181e49c56b6d29e48e2160a3c995a7127b45d94b
Abhishek1Mandal <101693497+Abhishek1Mandal@users.noreply.github.com>
2023-07-21 10:11:17 +0530

    my commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import {
-  Routes,
-  Route,
-  useNavigationType,
-  useLocation,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Page from "./page";
 import Page1 from "./page1";
 import Page2 from "./page2";
+import Login from "./Login";
 
 
 function App() {
@@ -16,6 +12,7 @@ function App() {
       <Route path="/" element={<Page/>} />
       <Route path="/page1" element={<Page1/>} />
       <Route path="/page2" element={<Page2/>} />
+      <Route path="/pop" element={<Login/>} />
     </Routes>
     
   );
